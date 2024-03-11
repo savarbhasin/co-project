5148d2bd307d2c884c8c5ff8ac0cf9cf37a6bd39
GolDRoger69 <141754279+GolDRoger69@users.noreply.github.com>
2024-03-11 23:37:23 +0530

    Add files via upload

---
+++ FIRST 3 B-TYPE INSTRUCTIONS.py
@@ -0,0 +1,35 @@
+# FIRST 3 B-TYPE INSTRUCTIONS
+
+# Define B-type command mappings
+B_type = {
+    "beq": {"opcode": "1100011", "funct3": "000"},
+    "bne": {"opcode": "1100011", "funct3": "001"},
+    "bge": {"opcode": "1100011", "funct3": "101"}
+}
+
+# Assemble B-type instructions
+def assemble_b_type_instruction(command, line_no):
+    t = command
+
+    if t[0] in B_type and len(t) == 4:
+        opcode = B_type[t[0]]["opcode"]
+        
+        funct3 = B_type[t[0]]["funct3"]
+
+        rs1 = Registers.get(t[2], None)
+        rs2 = Registers.get(t[1], None)
+
+        if rs1 is None or rs2 is None:
+            return "Syntax error: Invalid register name at line {line_no}"
+
+        imm_value = 0
+        if t[3].isdigit():
+            imm_value = int(t[3], 16)
+        else:
+            print("Invalid label Immidiate value at line {line_no}")
+
+        imm_binary = format(imm_value & 0xfff, '012b')
+
+        return imm_binary[11] + imm_binary[1:5] + rs2 + rs1 + funct3 + imm_binary[5:] + opcode
+    else:
+        return "Syntax error: Invalid B-type command or incorrect format"
